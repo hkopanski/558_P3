a0362450216d3db38d685436529856dd45676f22
hkopanski <52866254+hkopanski@users.noreply.github.com>
2021-07-19 13:08:47 -0400

    Update country_maps.R

--- country_maps.R
+++ country_maps.R
@@ -1,29 +1,35 @@
 library(tidyverse)
 library(maps)
+library(forcats)
 
-some.eu.countries <- c(
-  "Portugal", "Spain", "France", "Switzerland", "Germany",
-  "Austria", "Belgium", "UK", "Netherlands",
-  "Denmark", "Poland", "Italy", 
-  "Croatia", "Slovenia", "Hungary", "Slovakia",
-  "Czech republic"
-)
-# Retrievethe map data
-#some.eu.maps <- map_data("world", region = some.eu.countries)
-#some.eu.maps <- map_data("world")
-some.eu.maps <- map_data("world", region = c("China", "Thailand", "South Korea", "North Korea", 
-                                             "Vietnam", "Cambodia", "Laos", "Malaysia", "Myanmar", 
-                                             "Nepal", "India", "Bangladesh", "Mongolia", "Japan", 
-                                             "Brunei", "Pakistan", "Afghanistan", "Bhutan", "Indonesia"))
+df_coffee <- read_csv("Coffee-modified.csv")
 
-# Compute the centroid as the mean longitude and lattitude
-# Used as label coordinate for country's names
-region.lab.data <- some.eu.maps %>%
-  group_by(region) %>%
+df_coffee2 <- df_coffee %>% filter(!is.na(ID), !is.na(Country.of.Origin))
+
+df_coffee2 <- df_coffee2 %>% mutate(Country.of.Origin = recode(Country.of.Origin, 
+                                                               "Cote d?Ivoire" = "Ivory Coast",
+                                                               "United States" = "USA", 
+                                                               "Tanzania, United Republic Of" = "Tanzania",
+                                                               "United States (Hawaii)" = "Hawaii",
+                                                               "United States (Puerto Rico)" = "Puerto Rico"))
+
+coffee_countries <- unique(df_coffee2$Country.of.Origin)
+
+print(coffee_countries)
+
+coffee_maps <- map_data("world", region = coffee_countries)
+world_map <- map_data("world")
+                          
+region.lab.data <- coffee_maps %>% group_by(region) %>%
   summarise(long = mean(long), lat = mean(lat))
 
-ggplot(some.eu.maps, aes(x = long, y = lat)) +
-  geom_polygon(aes( group = group, fill = region)) +
-  geom_text(aes(label = region), data = region.lab.data,  size = 3, hjust = 0.5)+
+ggplot(coffee_maps, aes(x = long, y = lat)) +
+  geom_polygon(aes(group = group, fill = region)) +
+  #geom_polygon(aes(group = world_map)) +
+  #geom_text(aes(label = region), data = region.lab.data,  size = 3, hjust = 0.5)+
   scale_fill_viridis_d() +
   theme(legend.position = "none")
+
+ggplot(df_coffee2) + geom_bar(aes(fct_infreq(Country.of.Origin)), fill = 'maroon') +
+  labs(x = "Country of Origin", y = "Country Count", title = "Count of Each Country in the Data Set") +
+  theme(axis.text.x = element_text(angle = 30, hjust = 1))
